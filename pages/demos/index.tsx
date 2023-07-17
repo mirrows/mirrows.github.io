c4fafc96fc3ea11307db1e2e43453e4e7703d000
huangweiye <huangweiye@emodor.com>
2023-07-17 10:08:40 +0800

    demo静态化

--- pages/demos/index.tsx
+++ pages/demos/index.tsx
@@ -1,7 +1,7 @@
-import { stone } from '@/utils/global'
+import LazyImage from '@/components/LazyImage'
 import Head from 'next/head'
 import Link from 'next/link'
-import { useEffect, useRef } from 'react'
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
 const DIV = styled.div`
@@ -11,11 +11,74 @@ const DIV = styled.div`
     width: 100%;
     height: 100%;
     vertical-align: bottom;
-    z-index: -1;
+    z-index: 0;
+  }
+  .items_wrap{
+    position: relative;
+    z-index: 2;
+    display: grid;
+    justify-content: center;
+    grid-template-columns: repeat(auto-fill, 8rem);
+    grid-template-rows: repeat(auto-fill, 10rem);
+    min-width: 200px;
+    max-width: 1200px;
+    width: 100%;
+    padding: 10px 0;
+    margin: 60px auto;
+    pointer-events: none;
+  }
+  .item_wrap{
+    position: relative;
+    padding: 0.6rem;
+    pointer-events: all;
+  }
+  .item{
+    width: 100%;
+    height: unset;
+    padding: 10px;
+    border: 3px dashed #f5f5f5;
+    border-radius: 12px;
+    box-sizing: border-box;
+    overflow: hidden;
+    cursor: pointer;
+  }
+  .demo_name{
+    margin: 10px 0;
+    text-align: center;
+    word-break: break-all;
+    font-size: 14px;
+    color: #fff;
+    cursor: pointer;
+  }
+  @media (min-width: 769px) {
+    .item:hover ~ .item_mask{
+      opacity: 1;
+    }
   }
 `
 
 export default function Demos() {
+  const [show, isShow] = useState(false)
+  const [list] = useState([
+    {
+      name: '抽奖模型',
+      icon: 'https://empty.t-n.top/pub_lic/2023_06_19/pic1687153304035427.png',
+      link: '/demos/lottery',
+    },
+    {
+      name: '2048',
+      icon: 'https://empty.t-n.top/pub_lic/2023_06_26/pic1687749579236835.png',
+      link: '/demos/2048',
+    },
+    {
+      name: '图床列表',
+      icon: 'https://empty.t-n.top/pub_lic/2023_07_03/pic1688351230372957.jpg',
+      link: '/demos/imgSource',
+    },
+  ])
+  useEffect(() => {
+    isShow(true)
+  }, [])
   return (
     <>
       <Head>
@@ -26,9 +89,21 @@ export default function Demos() {
       </Head>
       <main>
         <DIV>
-          <iframe className='fire_wrap' src="https://empty.t-n.top/html/WebGL%20Fluid%20Simulation.html"></iframe>
+          {show && <iframe className='fire_wrap' src="https://empty.t-n.top/html/WebGL%20Fluid%20Simulation.html"></iframe>}
+          <div className="items_wrap">
+            {list.map((item, i) => (<Link key={i} className='item_wrap' aria-label={item.name} href={item.link}>
+              <LazyImage className='item' width="100" height="100" src={item.icon} />
+              <div className='demo_name two_line'>{item.name}</div>
+            </Link>))}
+          </div>
         </DIV>
       </main>
     </>
   )
+}
+
+export const getStaticProps = async (context: any) => {
+  return {
+    props: {}
+  }
 }
