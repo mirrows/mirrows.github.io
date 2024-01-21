257a587b43a42bca02974c2b66b14c6b7f214a8e
mirrows <huangweiye@chengyun.biz>
2024-01-21 09:21:26 +0800

    调整加载

--- pages/demos/imgSource/components/PicList/index.tsx
+++ pages/demos/imgSource/components/PicList/index.tsx
@@ -211,7 +211,6 @@ function UploadPicList({ list = [], mode = ModeMap.PHOTO, path = 'normal/', show
                       src={`https://wsrv.nl/?url=${(pic.download_url || '').replace('https://', '')}${mobile ? '&w=80&h=180' : '&w=300'}&fit=cover&n=-1&q=80`}
                       width="130"
                       height="320"
-                      noReload
                       onLoad={(src) => {
                         picLoaded(fold.path, i)
                       }}
