7a83a9a064c2674e3beba01a21a157bba4b59bff
huangweiye <huangweiye@emodor.com>
2025-01-09 14:19:42 +0800

    调整内容

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://example.com',
   generateIndexSitemap: false,
-  // generateRobotsTxt: true, // (optional)
+  generateRobotsTxt: true, // (optional)
+  exclude: [
+    '/blogs/create',
+    '/blogs/edit',
+    '/setting',
+  ]
   // ...other options
 }
